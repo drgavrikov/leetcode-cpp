43983467f6e1092834bd396f2fce39507fafaac2
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-27 14:03:25 +0400

    problem-356

--- Problem356.cpp
+++ Problem356.cpp
@@ -10,25 +10,31 @@ using std::numeric_limits;
 class Problem356 {
 public:
     bool isReflected(vector<vector<int> > &points) {
-        unordered_map<int, unordered_set<int> > coordinates;
+        unordered_map<int, unordered_set<int>> coordinates;
         int maxX = numeric_limits<int>::min();
         int minX = numeric_limits<int>::max();
 
-        for (const vector<int>& point : points) {
+        for (const auto &point: points) {
             const int x = point[0];
             const int y = point[1];
             coordinates[x].insert(y);
 
-            if (x > maxX) maxX = x;
-            if (x < minX) minX = x;
+            if (x > maxX) {
+                maxX = x;
+            }
+            if (x < minX) {
+                minX = x;
+            }
         }
 
         double middle = (maxX + minX) / 2.0;
-        for (const vector<int>& point : points) {
+        for (const auto &point: points) {
             const int x = point[0];
             const int y = point[1];
             const int reflectedX = static_cast<int>(middle - (x - middle));
-            if (coordinates.count(reflectedX) == 0 || coordinates[reflectedX].count(y) == 0) return false;
+            if (coordinates.count(reflectedX) == 0 || coordinates[reflectedX].count(y) == 0) {
+                return false;
+            }
         }
         return true;
     }
@@ -36,7 +42,10 @@ public:
 
 int main() {
     Problem356 problem356;
-    vector<vector<int>> points = {{1, -1}, {1, 1}};
+    vector<vector<int>> points = {
+            {1, -1},
+            {1, 1}
+    };
     assert(problem356.isReflected(points));
     return 0;
 }
