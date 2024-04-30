291640ddfd57b5d834b8d3db7028506263999677
dr_gavrikov <agavrikov1989@gmail.com>
2024-05-01 02:06:00 +0400

    problem-1915

---
+++ src/Problem1915.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <cassert>
+
+/**
+ * @author Aleksandr Gavrikov
+ * @url https://leetcode.com/problems/number-of-wonderful-substrings/solutions/5093288/bit-manipulation-solution-with-linear-complexity/
+ */
+class Problem1915 {
+public:
+    long long wonderfulSubstrings(std::string word) {
+        const int size = 1 << 10;
+        long long frequencies[size];
+        std::fill(frequencies, frequencies + size, 0LL);
+
+        frequencies[0] = 1;
+
+        long long result = 0;
+
+        int mask = 0;
+        for (int i = 0; i < word.length(); ++i) {
+            int bit = word[i] - 'a';
+            mask ^= (1 << bit);
+
+            result += frequencies[mask];
+            for (int odd_bit = 0; odd_bit < 10; ++odd_bit) {
+                result += frequencies[mask ^ (1 << odd_bit)];
+            }
+            frequencies[mask]++;
+        }
+        return result;
+    }
+};
+
+int main() {
+    Problem1915 problem;
+    assert(4 == problem.wonderfulSubstrings("aba"));
+    assert(9 == problem.wonderfulSubstrings("aabb"));
+    return 0;
+}
